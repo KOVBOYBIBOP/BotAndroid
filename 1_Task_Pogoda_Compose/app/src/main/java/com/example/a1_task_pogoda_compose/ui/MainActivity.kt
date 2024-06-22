807be3f2cb490f3cbd325fbdd81bfb25b139763e
Daniil <barsukgeroi03@gmail.com>
2024-06-22 19:19:20 +0300

    Поменял картинки

---
+++ 1_Task_Pogoda_Compose/app/src/main/java/com/example/a1_task_pogoda_compose/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.a1_task_pogoda_compose.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.a1_task_pogoda_compose.ui.ui.MainScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
